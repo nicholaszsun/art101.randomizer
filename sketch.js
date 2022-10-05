7cc85ba906e43cf25be2572e07abf02e32dbfc9f
nicholasz-sun <nicholas.sun@sjsu.edu>
2022-10-04 23:19:35 -0700

    added

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//Create array of Computer Parts
+let pcparts = ["CPU","GPU","RAM","MOTHERBOARD","SSD","HDD"]
+
+function setup() {
+  createCanvas(600,600)
+  background(230)
+
+  pcparts[0]="Central Proccesing Unit";
+  console.log();
+
+}
+
+function draw(){
+
+}
